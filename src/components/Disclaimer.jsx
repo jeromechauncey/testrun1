0495fbf7d06d828aa7ffa1602214cea52f466ea2
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-20 23:13:24 +0300

    Adds:Disclaimer

---
+++ src/components/Disclaimer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Disclaimer() {
+  return (
+    <div className="bg-dark text-white sticky-top">
+      <div className="container px-3 px-lg-0 ">
+        <div className="d-flex justify-content-center">
+          <div className="text-start">
+            <p>
+              Online Forex/CFDs are complex instruments and come with a high
+              risk of losing money rapidly due to leverage. 84.62% of retail
+              investor accounts lose money when trading Online Forex/CFDs. You
+              should consider whether you understand how CFDs work and whether
+              you can afford to take the high risk of losing money.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Disclaimer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Disclaimer from '../components/Disclaimer';
 
 function Home(){
     return(
         <div>
+            <Disclaimer/>
             <p>Home</p>
         </div>
     )
